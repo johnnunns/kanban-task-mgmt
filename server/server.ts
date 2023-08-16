05456b1698e38d48081b3bb79c43ef544c01de92
John Nunns <JohnDNunns@gmail.com>
2023-08-15 18:21:52 -0700

    tsconfig updates

--- server/server.ts
+++ server/server.ts
@@ -1,25 +1,20 @@
 import 'express-async-errors'
 import express from 'express';
 const app = express();
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 dotenv.config();
 import morgan from 'morgan';
 import path from 'path';
 
-// dn and authenticate user
 import connectDB from './db/connect';
 
-// routers
 import boardRouter from './routes/boardRoutes';
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-// only when ready to deploy
-// app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(express.json());
 
@@ -29,10 +24,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1', boardRouter);
 
-// only when ready to deploy
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 
